f6d3d1f006e7532aca119757721e47855e1dc6cb
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-24 18:06:14 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:index,:show]
+  resources :items, only: [:index,:show,:create,:destroy]
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
 end
