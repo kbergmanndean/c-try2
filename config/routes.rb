59e2a0413a42c0a8607c75d277cbf291bbc40bfa
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-25 15:35:33 -0500

    added back root path

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   # Defines the root path route ("/")
-  # root "fallback#index"
+  root to: "fallback#index"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
