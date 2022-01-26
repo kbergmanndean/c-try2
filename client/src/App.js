65b90d17835bafefc3b87b90589c61d4289235b6
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-26 17:10:30 -0500

    now using hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from "./Components/Home.js"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
