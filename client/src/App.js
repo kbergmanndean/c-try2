fd45b3f856e1c99d4fb7be15e3f84053f4bb9ad2
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-31 19:25:03 -0500

    new node buildpack

--- client/src/App.js
+++ client/src/App.js
@@ -6,11 +6,9 @@ import {Routes, Route} from "react-router-dom"
 function App() {
   return (
     <div className="App">
-      
         <Routes>
           <Route exact path="/" element={<Home/>}/>
         </Routes>
-      
     </div>
   );
 }
