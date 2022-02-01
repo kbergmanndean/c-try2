bc60c6de337ee21f977ff3acb8d3bc273f1e8e89
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-01-31 19:18:00 -0500

    change location of hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import './App.css';
 import Home from "./Components/Home.js"
-import {HashRouter as Router, Routes, Route} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Router basename="/">
+      
         <Routes>
           <Route exact path="/" element={<Home/>}/>
         </Routes>
-      </Router>
+      
     </div>
   );
 }
